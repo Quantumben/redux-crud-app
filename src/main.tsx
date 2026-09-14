e61335969780ccf0c6d27937b0ffb701b654d98e
QuantumBen <omosabenedict@gmail.com>
2026-09-14 22:58:41 +0100

    Give React access to Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import { store } from "./app/store";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+);
+
+// Provider saying:
+//Every React component underneath me is allowed to access this Redux store.
+
+//without Provider, this won't work: useAppSelector(...)
